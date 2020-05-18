9597edf3e3949b92913da1c825db6b9532bb86dc
Amir A. Shabani <amirshabani011@gmail.com>
2020-05-18 19:55:17 +0430

    Add setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="dpern",
+    version="0.0.1",
+    description="Describe PERsian Numbers",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amirashabani/dpern",
+    author="Amir A. Shabani",
+    author_email="amirashabani@zoho.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=["dpern"],
+    setup_requires=["wheel"]
+)
